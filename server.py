8086b91804d2c044f60765753db69a1d120a2883
DannySliekers <dannysliekers@hotmail.com>
2021-09-18 16:41:24 +0200

    Dont close server socket after one message

--- server.py
+++ server.py
@@ -6,10 +6,11 @@ PORT = 4444
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, adress = s.accept()
-    with conn:
-        print('Connected by', adress)
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+    while True:
+        conn, adress = s.accept()
+        with conn:
+            print('Connected by', adress)
+            while True:
+                data = conn.recv(1024)
+                if not data: break
+                conn.sendall(data)
