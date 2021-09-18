37c9d0fe98fd7e1ad7c6b5234913b3a4fc7b4b35
DannySliekers <dannysliekers@hotmail.com>
2021-09-18 06:32:20 +0200

    first commit

---
+++ client.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+def send_message():
+    chat_box.configure(state=NORMAL)
+    message = input.get()
+    chat_box.insert(END, message + '\n')
+    chat_box.configure(state=DISABLED)
+
+window = Tk()
+window.title("Chatter")
+
+input = Entry(window, width=120)
+input.grid(row=1, column=0)
+
+send = Button(window, text="Send", width=10, command=send_message)
+send.grid(row=1, column=1)
+
+chat_box = Text(window, height=20, width=100)
+chat_box.configure(state=DISABLED)
+chat_box.grid(row=0, column=0, columnspan=2)
+
+window.mainloop()
