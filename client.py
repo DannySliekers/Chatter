7bf494bd27d904193277d54779178bb2b7c1c2ae
DannySliekers <dannysliekers@hotmail.com>
2021-09-18 18:58:47 +0200

    Added methods and a message queue, still needs fix

--- client.py
+++ client.py
@@ -4,21 +4,30 @@ import socket
 HOST = 'localhost'
 PORT = 4444
 
-def connect():
+def send_message():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(bytes(input.get(), 'utf-8'))
-        data = s.recv(1024).decode('utf-8')
-        name = str(s.getsockname())
-        return data, name
+        if not input.get():
+            s.sendall(b' ')
+        else:
+            s.sendall(bytes(input.get(), 'utf-8'))
 
-def send_message():
+def print_message():
+    message, name = get_messages()
     chat_box.configure(state=NORMAL)
-    message, name = connect()
     formatted_output = name + ': ' + message + '\n'
     chat_box.insert(END, formatted_output)
     input.delete(0, END)
     chat_box.configure(state=DISABLED)
+    window.after(500, print_message)
+
+def get_messages():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        message = s.recv(1024).decode('utf-8')
+        name = str(s.getsockname())
+        return message, name
+
 
 window = Tk()
 window.title("Chatter")
@@ -33,4 +42,5 @@ chat_box = Text(window, height=20, width=100)
 chat_box.configure(state=DISABLED)
 chat_box.grid(row=0, column=0, columnspan=2)
 
+window.after(500, print_message)
 window.mainloop()
--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import socket
 HOST = ''
 PORT = 4444
 
+message_queue = []
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -11,6 +13,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print('Connected by', adress)
             while True:
+                if not message_queue:
+                    conn.sendall(b' ')
+                else:
+                    for message in message_queue:
+                        conn.sendall(message_queue.pop(0))
                 data = conn.recv(1024)
+                message_queue.append(data)
                 if not data: break
-                conn.sendall(data)
