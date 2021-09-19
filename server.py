eef310a32f4f7e1b80ed7a8f197a5724542ac113
DannySliekers <dannysliekers@hotmail.com>
2021-09-19 22:44:21 +0200

    every client socket on the server now has its own thread

--- server.py
+++ server.py
@@ -1,23 +1,29 @@
 import socket
+import threading
 
 HOST = ''
 PORT = 4444
 
 message_queue = []
 
+def handle_messages(conn):
+    with conn:
+        if not message_queue:
+            conn.sendall(b' ')
+        else:
+            for message in message_queue:
+                conn.sendall(message_queue.pop(0))
+        data = conn.recv(1024)
+        if data != b' ':
+            message_queue.append(data)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     while True:
         conn, adress = s.accept()
-        with conn:
-            print('Connected by', adress)
-            if not message_queue:
-                conn.sendall(b' ')
-            else:
-                for message in message_queue:
-                    conn.sendall(message_queue.pop(0))
-            data = conn.recv(1024)
-            if data != b' ':
-                message_queue.append(data)
-            if not data: break
+        print('Connected by', adress)
+        client_thread = threading.Thread(target=handle_messages(conn))
+        client_thread.start()
+
+    
