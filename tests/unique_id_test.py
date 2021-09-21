c509afca77fff6cace49cce6b5e530e8612d2cf5
DannySliekers <dannysliekers@hotmail.com>
2021-09-21 23:59:04 +0200

    linter

--- tests/unique_id_test.py
+++ tests/unique_id_test.py
@@ -1,6 +1,12 @@
 from chatter import unique_id
 import re
 
-# Tests if the returned unique_id only has alphanumeric chars and if its exactly 24 chars long
+
+""" Tests if the returned unique_id
+only has alphanumeric chars and if its exactly 24 chars long """
+
+
 def test_unique_id():
-    assert re.match("^[a-zA-Z0-9]{24}$", unique_id.generate_unique_id())
+    id = unique_id.generate_unique_id()
+    regex = "^[a-zA-Z0-9]{24}$"
+    assert re.match(regex, id)
